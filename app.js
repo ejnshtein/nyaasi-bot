bde8c009afaf50b1cbbe6d966b9e9aca42096f09
Okabe Rintaro <ejnshtein@gmail.com>
2018-09-12 23:41:50 +0300

    Merge branch 'master' of https://github.com/ejnshtein/nyaasi-bot

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ bot.command('count', async (ctx) => {
 bot.command('source', (ctx) => {
     ctx.reply('My source code at <a href="https://github.com/ejnshtein/nyaasi-bot">Github</a>',{
         parse_mode: 'HTML',
+        disable_web_page_preview: true,
         reply_markup: {
             inline_keyboard: [
                 [
@@ -55,7 +56,7 @@ bot.command('source', (ctx) => {
     })
 })
 
-bot.command('about', (ctx) => ctx.reply('I\'m <a href="https://nyaa.si">nyaa.si</a> website bot.\nFor now, I can search for torrents on <a href="https://nyaa.si">nyaa.si</a> (＾◡＾)っ.\nMore features will arrive soon! ( ͡~ ͜ʖ ͡°)\n\nI\'m still in beta, so please be patient! ( ﾉ ﾟｰﾟ)ﾉ', {
+bot.command('about', (ctx) => ctx.reply('I\'m <a href="https://nyaa.si">nyaa.si</a> website bot.\nFor now, I can search for torrents on <a href="https://nyaa.si">nyaa.si</a> (＾◡＾)っ.\nMore features will arrive soon! ( ͡~ ͜ʖ ͡°)\n\nI\'m still in beta, so please be patient! ( ﾉ ﾟｰﾟ)ﾉ\n\nMy source code at <a href="https://github.com/ejnshtein/nyaasi-bot">github</a>', {
     parse_mode: 'HTML'
 }))
 
