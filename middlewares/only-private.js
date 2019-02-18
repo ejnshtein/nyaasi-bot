c14072348a2bfddbd902e1546c445b0171fe4b92
Okabe Rintaro <ejnshtein@gmail.com>
2019-02-19 00:26:20 +0200

    onlyPrivate middleware fixes

--- middlewares/only-private.js
+++ middlewares/only-private.js
@@ -1,6 +1,10 @@
-module.exports = async ({ chat, reply }, next) => {
+module.exports = async ({ chat }, next) => {
   if (chat.type === 'private') {
-    next()
+    if (typeof next === 'function') {
+      next()
+    } else {
+      return true
+    }
   }
   // else if (chat.type === 'group' || chat.type === 'supergroup') {
   //   reply('This bot works only in private chat.')
